79c5ae4d891b8d8097f3d6da47fa262444e64bd1
Volodymyr Mazepa <v.mazepa@gmail.com>
2023-05-06 01:20:08 +0300

    new build of Web version

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,9 +22,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "83591a9f65892bca3d056591e308d325",
 "icons/Icon-maskable-192.png": "9c265701fb433c56b1cf90268ed427fd",
 "icons/Icon-maskable-512.png": "83591a9f65892bca3d056591e308d325",
-"index.html": "6db6dc24667484fbe908fdf9f0f14b13",
-"/": "6db6dc24667484fbe908fdf9f0f14b13",
-"main.dart.js": "a75cb068896c5f9c70ac68230d2784bf",
+"index.html": "8f6f2c51a3fec5c2cce9770dac3fe6b2",
+"/": "8f6f2c51a3fec5c2cce9770dac3fe6b2",
+"main.dart.js": "727ac6bb1cd1f912af3c5182cb645f7f",
 "manifest.json": "01f395258107b10e1a78cf655ef79f00",
 "version.json": "e39d08dc54c7a46720c82eb1fc5d95b7"
 };
