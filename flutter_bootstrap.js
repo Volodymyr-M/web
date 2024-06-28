bd436b2b34951334265e45f158dc5933feba6801
Volodymyr-M <64850494+Volodymyr-M@users.noreply.github.com>
2024-06-29 00:19:13 +0300

    new build of Web version

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3542120034"
+    serviceWorkerVersion: "1166844450"
   }
 });
