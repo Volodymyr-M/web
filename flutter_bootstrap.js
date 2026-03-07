bc6065b89f01d14c8117d5497eae7db6646612db
Volodymyr Mazepa <v.mazepa@gmail.com>
2026-03-08 01:08:37 +0200

    new build of Web version

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2298437980" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1050925154" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
